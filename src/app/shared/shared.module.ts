bf91746d3972c34ff385bc78a39e5580e47e8096
Dora <dora.sparagno@gmail.it>
2024-08-26 19:47:54 +0200

    "Updated angular.json, added sidenav component, modified app component HTML, updated shared module, modified index.html, and updated styles.scss"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { AngularMaterialModule } from './angularMaterial/angular-material.module';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidenavComponent
+  ],
   imports: [
     CommonModule,
     SharedRoutingModule,
     AngularMaterialModule
-  ]
+  ],
+  exports:[SidenavComponent]
 })
 export class SharedModule { }
---
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { ThemeManagerService } from '../../services/theme-manager.service';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss'
+})
+export class SidenavComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  overlay!: boolean;
+  private themeManager = inject(ThemeManagerService);
+  theme = this.themeManager.theme;
+
+  toggleTheme() {
+    this.themeManager.toggleTheme();
+    console.log('Current theme:', this.theme());
+  }
+}
