b53b370f26e0458b148fbf775806461ef620e83e
Dora <dora.sparagno@gmail.it>
2024-09-05 20:12:45 +0200

    "Updated CO2 component HTML, SCSS, and TS files; refactored sidenav component HTML and SCSS; added dark theme styling for side menu items and dynamic paragraphs."

--- src/app/pages/co2/co2.component.ts
+++ src/app/pages/co2/co2.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Co2Service } from '../../services/co2.service';
 import { Co2ApiResponse, Co2Data } from '../../models/co2Data.model';
 import { ClientAPIService } from '../../services/client-api.service';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as echarts from 'echarts';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-co2',
@@ -22,6 +22,7 @@ export class Co2Component implements OnInit {
     private dataService: DataService,
     private Co2Service: Co2Service,
     private clientApi: ClientAPIService,
+    @Inject(PLATFORM_ID) private platformId: object,
   ) {}
 
   ngOnInit(): void {
@@ -30,71 +31,76 @@ export class Co2Component implements OnInit {
     this.dataService.changeContent(this.contentCo2);
     this.clientApi.getCo2Data().subscribe((response: Co2ApiResponse) => {
       this.co2Data = response.co2;
-      this.createChart();
+      if (isPlatformBrowser(this.platformId)) {
+        this.createChart();
+      }
     });
   }
 
   createChart() {
-    const dates = this.co2Data.map(
-      (data) => `${data.day}-${data.month}-${data.year}`,
-    );
-    const cycles = this.co2Data.map((data) => data.cycle);
-    const trends = this.co2Data.map((data) => data.trend);
+    if (isPlatformBrowser(this.platformId)) {
+      const chartDom = document.getElementById('echart');
+      const myChart = echarts.init(chartDom, null, {
+        width: 'auto',
+        height: 'auto'
+      });
+      const dates = this.co2Data.map(
+        (data) => `${data.day}-${data.month}-${data.year}`,
+      );
+      const cycles = this.co2Data.map((data) => data.cycle);
+      const trends = this.co2Data.map((data) => data.trend);
 
-    this.chartOption = {
-      title: {
-        text: 'CO2 Levels',
-        textStyle: {
-          color: "rgba(143, 22, 22, 0.39)"
+      this.chartOption = {
+        tooltip: {
+          trigger: 'axis',
         },
-      },
-      tooltip: {
-        trigger: 'axis',
-      },
-      legend: {
-        data: ['Cycle', 'Trend'],
-      },
-      xAxis: {
-        type: 'category',
-        data: dates,
-      },
-      yAxis: {
-        name: 'CO2 Levels (ppm)',
-        min: 350,
-        max: 500,
-        axisLabel: {
-          formatter: '{value} ppm',
+        legend: {
+          data: ['Cycle', 'Trend'],
+          padding: [0, 40],
         },
-      },
-      series: [
-        {
-          name: 'Cycle',
-          type: 'line',
-          data: cycles,
-          showSymbol: false,
-          encode: {
-            x: 'Year',
-            y: 'Cycle',
-            itemName: 'Year',
-            tooltip: ['Cycle'],
+        xAxis: {
+          type: 'category',
+          data: dates,
+        },
+        yAxis: {
+          name: 'CO2 Levels (ppm)',
+          min: 350,
+          max: 450,
+          axisLabel: {
+            formatter: '{value}',
           },
         },
-        {
-          name: 'Trend',
-          type: 'line',
-          data: trends,
-          showSymbol: false,
-          encode: {
-            x: 'Year',
-            y: 'Trend',
-            itemName: 'Year',
-            tooltip: ['Trend'],
+        series: [
+          {
+            name: 'Cycle',
+            type: 'line',
+            data: cycles,
+            showSymbol: false,
+            encode: {
+              x: 'Year',
+              y: 'Cycle',
+              itemName: 'Year',
+              tooltip: ['Cycle'],
+            },
           },
-        },
-      ],
-    };
-   
+          {
+            name: 'Trend',
+            type: 'line',
+            data: trends,
+            showSymbol: false,
+            encode: {
+              x: 'Year',
+              y: 'Trend',
+              itemName: 'Year',
+              tooltip: ['Trend'],
+            },
+          },
+        ],
+      };
+      myChart.setOption(this.chartOption);
+      window.addEventListener('resize', () => {
+        myChart.resize();
+      });
+    }
   }
-
-
 }
